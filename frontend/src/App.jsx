f849688545c27f27a844055e07a6d036c93d300e
Cash Yang <CashYang16@gmail.com>
2026-05-17 11:52:01 +0800

    Add docker

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const [isSending, setIsSending] = useState(false);
 
   const placeholderTips = useMemo(
-    () => ["過去五天成交量下降", "產生一個均值回歸策略的alpha",],
+    () => ["過去五天成交量下降", "產生一個alpha並使用rmse當作fitness function",],
     []
   );
 
